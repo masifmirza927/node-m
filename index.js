acd7004eeacab62860e9bced8eee98f08fffa813
masifmirza927 <masifmirza927@gmail.com>
2023-06-07 11:43:17 +0500

    fs mofule functions

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 // const  { sum, multiply } = require("./functions");
-const os = require("os");
+// const os = require("os");
+const fs = require("fs");
+
+// os module functions
 
 // console.log(os.type());
 // console.log(os.uptime());
 // console.log(os.cpus().length)
 
+
+// fs module functions
+// fs.writeFile("sample.txt", "hello from node js", (err) => {
+//     if(err) {
+//         console.log(err)
+//     }else {
+//         console.log("file created");
+//     }
+
+// });
+
+// delete file
+// fs.unlink("./sample.pdf", () => {
+//     console.log("file deleted");
+// })
+
+// create new folder
+// fs.mkdir("files", () => {
+//     console.log("folder created successfully");
+// })
 
 
 // sum(3, 5)
