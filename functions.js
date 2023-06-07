fef67bf43b2e2229bb9ba1effb4078686bae1120
masifmirza927 <masifmirza927@gmail.com>
2023-06-07 11:09:07 +0500

    first commit

---
+++ functions.js
@@ -0,0 +1,10 @@
+
+function sum(value1, value2) {
+    console.log(value1 + value2)
+}
+
+function multiply(value1, value2) {
+    console.log(value1 * value2);
+}
+
+module.exports = {sum, multiply};
---
+++ index.js
@@ -0,0 +1,5 @@
+const  { sum, multiply } = require("./functions");
+
+
+sum(3, 5)
+multiply(5, 6);
