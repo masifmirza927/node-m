808f59f7169dfdc18744bbd50ec9b1b706933dcd
masifmirza927 <masifmirza927@gmail.com>
2023-06-07 11:19:47 +0500

    os module

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const  { sum, multiply } = require("./functions");
+// const  { sum, multiply } = require("./functions");
+const os = require("os");
+
+// console.log(os.type());
+// console.log(os.uptime());
+// console.log(os.cpus().length)
 
 
-sum(3, 5)
-multiply(5, 6);
+
+// sum(3, 5)
+// multiply(5, 6);
