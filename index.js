481550b4e66bf809e047478698309ae00cd27b06
masifmirza927 <masifmirza927@gmail.com>
2023-06-08 10:47:56 +0500

    create server

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-// const  { sum, multiply } = require("./functions");
-// const os = require("os");
-const fs = require("fs");
+const http = require("http");
 
-// os module functions
+const server = http.createServer( (request, response) => {
+   
+    if(request.url == "/about") {
+        response.write("this is about page");
+    }else if(request.url == "/") {
+        response.write("this is home page");
+    }
 
-// console.log(os.type());
-// console.log(os.uptime());
-// console.log(os.cpus().length)
+    response.end();
 
+});
 
-// fs module functions
-// fs.writeFile("sample.txt", "hello from node js", (err) => {
-//     if(err) {
-//         console.log(err)
-//     }else {
-//         console.log("file created");
-//     }
-
-// });
-
-// delete file
-// fs.unlink("./sample.pdf", () => {
-//     console.log("file deleted");
-// })
-
-// create new folder
-// fs.mkdir("files", () => {
-//     console.log("folder created successfully");
-// })
-
-
-// sum(3, 5)
-// multiply(5, 6);
+// http://localhost:3002
+server.listen(3002, () => {
+    console.log("server is running...")
+});
