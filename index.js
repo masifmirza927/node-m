0880960cf6dbe73e94acff48c92affa51edae74a
masifmirza927 <masifmirza927@gmail.com>
2023-06-12 10:25:19 +0500

    query params

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ app.get("/", (request, response) => {
     response.json(student);
   })
 
+  app.get("/search-fruit", (request, response) => {
+    console.log(request.query.id);
+
+      const fruits = ["apple", "mango", "banana"];
+      const result = fruits[1];
+
+    response.json({
+      status: "OK",
+      result: result
+    });
+  })
+
+  app.get("/search/:id", (request, response) => {
+    console.log(request.params.id)
+    response.json({
+      status: "OK"
+    });
+
+  })
+
 app.listen(port, () => {
     console.log(` app listening on port ${port}`)
   })
