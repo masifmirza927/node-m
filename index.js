8a61e62447391e103871e470a8c2cc2ec650fb56
masifmirza927 <masifmirza927@gmail.com>
2023-06-16 10:28:17 +0500

    update query

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ const StudentModel = require("./models/StudentModel");
 // middleware
 app.use(express.json());
 
-app.get("/",  async (request, response) => {
+app.get("/students",  async (request, response) => {
 
      console.log("line 11, top")
-      const student =  await StudentModel.findById("6489626e4c7eefde2348ed28");
-      
-      console.log(student);
-      console.log("line 15, bottom")
+      const students =  await StudentModel.find();
+
+      response.json({
+        "status": true,
+        students: students
+      })
 
 })
+
+app.post("/create-student", async (request, response) => {
+        const newStudent = request.body;
+        await StudentModel.create(newStudent);
+
+        response.json({
+          "status": true,
+          "msg": 'successfully created'
+        })
+})
+
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/studentsDbM').then(() => {
   app.listen(3003, () => {
